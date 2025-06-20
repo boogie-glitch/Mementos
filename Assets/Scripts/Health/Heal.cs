45ed2e64311e0723020d17ac107397fca644d155
boogie-glitch <thanhhai3009hutech@gmail.com>
2025-06-20 18:06:43 +0700

    Heal for Player: Done

---
+++ Assets/Scripts/Health/Heal.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Heal : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerHealth playerHealth;
+    [SerializeField]
+    private Image[] healIcons;
+    [Range(1, 100)]
+    public int healAmount = 35; // Heal amount per use
+    private int healUsesLeft = 3;
+
+    [SerializeField]
+    private float healCooldown = 1f; // Cooldown time in seconds
+    [SerializeField]
+    private float healWaiting = 2f; // Waiting time before the next heal can be used
+    [SerializeField]
+    private bool canUseHeal = true; // Whether the player can use heal
+
+    private void Start()
+    {
+        UpdateHealIcons();
+    }
+
+    public void OnHeal(InputAction.CallbackContext context)
+    {
+        if (healUsesLeft == 0)
+        {
+            return; // No uses left, do nothing
+        }
+
+        if (!context.started)
+        {
+            return; // Only trigger on action start
+        }
+
+        if (!canUseHeal)
+        {
+            return; // Heal is on cooldown or not allowed
+        }
+
+        if (playerHealth.Hp == playerHealth.MaxHp)
+        {
+            return; // Player is already at full health
+        }
+
+        StartCoroutine(HealCooldownRoutine());
+    }
+
+    private void UpdateHealIcons()
+    {
+        for (int i = 0; i < healIcons.Length; i++)
+        {
+            healIcons[i].enabled = i < healUsesLeft;
+        }
+    }
+    private System.Collections.IEnumerator HealCooldownRoutine()
+    {
+        canUseHeal = false;
+        yield return new WaitForSeconds(healWaiting);
+
+        playerHealth.Heal(healAmount);
+        healUsesLeft--;
+
+        UpdateHealIcons();
+
+        EnableHeal(); // Re-enable heal input if there are uses left
+
+        //yield return new WaitForSeconds(healCooldown);
+        canUseHeal = true; // Allow heal to be used again after cooldown
+    }
+
+    private void EnableHeal()
+    {
+        if (healUsesLeft <= 0)
+        {
+            // Disable heal input if no uses left
+            var playerInput = GetComponent<PlayerInput>();
+            if (playerInput != null)
+            {
+                playerInput.actions["Heal"].Disable();
+            }
+        }
+        else
+        {
+            // Re-enable heal input if there are uses left
+            var playerInput = GetComponent<PlayerInput>();
+            if (playerInput != null)
+            {
+                playerInput.actions["Heal"].Enable();
+            }
+        }
+    }
+}
